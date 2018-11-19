066e2ea8e08a94eb0c621e81af31a8a028781f7f
adamSales <asales@utexas.edu>
2018-11-19 17:09:32 -0600

    FOD and occupation code, from mark bond

--- ACS2017/attainment.r
+++ ACS2017/attainment.r
@@ -1,85 +1,135 @@
-library(gridExtra)
-library(ggplot2)
-library(cluster)
-library(readr) ## read in the csvs faster
-library(survey)
 library(dplyr)
-library(openxlsx)
-states <- read.csv('../../../data/states.csv')
+library(broom)
 
-#1) a simple breakdown of current enrollment, and completion data, across type of institution (4 year colleges, community colleges, etc) using all the 'type of institution' data we have, so that would give us some nice descriptives and allow us to make a final decision on how we want to categorize 'community colleges and 2-year institutions'
-
-
-
-
-varNames <- c('ST','AGEP','DDRS','DEAR','DEYE','DOUT','DPHY','DRATX','DREM','FDEARP','ESR','SCHL','RAC1P','HISP','SEX','PERNP','PINCP','SSIP','WKHP','WKW','ADJINC','PWGTP','RELP',paste0('PWGTP',1:80))
-
-firstTry <- read_csv(paste0('../../../data/byYear/ss17pusa.csv'), n_max=5)
-colTypes <- ifelse(names(firstTry)%in%varNames,'i','-')
-missingVars <- setdiff(varNames,names(firstTry)[colTypes=='i'])
-if(length(missingVars)) cat('WARNING: Missing these variables:\n',missingVars,'\n')
-
-colTypes <- paste(colTypes,collapse='')
+if(!exists("dat")) load('attainmentEmploymentData.RData')
+source('../generalCode/estimationFunctions.r')
+source('../generalCode/median.r')
 
 
-datA <- read_csv('../../../data/byYear/ss17pusa.csv',col_types=colTypes)
-datB <- read_csv('../../../data/byYear/ss17pusb.csv',col_types=colTypes)
-## need: DEAR, attain, employment,PERNP, fulltime
-dat <- rbind(datA[,varNames],datB[,varNames])
+FIX <- function(tib){
+    lst <- sapply(tib,is.list)
 
-rm(datA,datB); gc()
+    out <- NULL
+    for(nn in names(tib)[lst]){
+        out <- round(cbind(out,
+                     do.call('rbind',tib[[nn]])),1)
+    }
+    out <- cbind(tib[,!lst],out)
+    names(out)[1:sum(!lst)] <- ''
+    out
+}
 
-names(dat) <- tolower(names(dat))
+FIX2 <- function(tib){
+    tib <- FIX(tib)
+    out <- t(tib[,-c(1,ncol(tib))])
+    colnames(out) <- tib[,1]
+    out
+}
 
-dat$state <- states$abb[match(dat$st,states$x)]
-
-
-edlevs <- c(
-    '<Grade 10',
-    'Grade 10',
-    'Grade 11',
-    '12th grade - no diploma',
-    'Regular high school diploma',
-    'GED or alternative credential',
-    'Some college, but less than 1 year',
-    '1 or more years of college credit, no degree',
-    'Associates degree',
-    'Bachelors degree',
-    'Masters degree',
-    'Professional degree beyond a bachelors degree',
-    'Doctorate degree')
-
-dat$attain <- ifelse(dat$schl<13,1,dat$schl-11)
-dat$attain <- factor(edlevs[dat$attain],levels=edlevs,ordered=TRUE)
+standard <- function(x,FUN,dat,...)
+    list(
+        overall=FIX(dat%>%group_by(deaf)%>%do(x=FUN(x,.))),
+        byAge=FIX(dat%>%group_by(deaf,Age)%>%do(x=FUN(x,.))),
+        bySex=FIX(dat%>%group_by(deaf,sex)%>%do(x=FUN(x,.))),
+        byRace=FIX(dat%>%group_by(deaf,raceEth)%>%do(x=FUN(x,.))),
+        byRaceGender=FIX(dat%>%group_by(deaf,raceEth,sex)%>%do(x=FUN(x,.))),
+        byDiss=rbind(
+            FIX(dat%>%group_by(deaf,diss)%>%do(x=FUN(x,.))),
+            FIX(dat%>%group_by(deaf,blind)%>%do(x=FUN(x,.))),
+            FIX(dat%>%group_by(deaf,selfCare)%>%do(x=FUN(x,.))),
+            FIX(dat%>%group_by(deaf,indLiv)%>%do(x=FUN(x,.))),
+            FIX(dat%>%group_by(deaf,amb)%>%do(x=FUN(x,.))),
+            FIX(dat%>%group_by(deaf,servDis)%>%do(x=FUN(x,.))),
+            FIX(dat%>%group_by(deaf,cogDif)%>%do(x=FUN(x,.)))),
+        ...
+    )
 
 
 
-dat <- dat%>%filter(agep>24,agep<65,relp!=16)%>% ## relp==16 for institutionalized
-    mutate(
-        deaf=ifelse(dear==1,1,0),
-        Age=ifelse(agep<35,'25-34',
-            ifelse(agep<45,'35-44',
-            ifelse(agep<55,'45-54','55-64'))),
-        attainCum=ifelse(attain<'Regular high school diploma','No HS',
-                  ifelse(attain<'Some College, but less than 1 year','HS Diploma',
-                  ifelse(attain<'Associates degree','Some College',
-                  ifelse(attain<'Bachelors degree','Associates',
-                  ifelse(attain<'Masters degree','Bachelors','Post-Graduate'))))),
-        employment=ifelse(esr%in%c(1,2,4,5),'Employed',
-                   ifelse(esr==3,'Unemployed','Not In Labor Force')),
+attainment <- standard('attainCum',factorProps,dat)
 
-        fulltime=(employment=='Employed')&(wkw==1 & wkhp>=35),
+employment <- standard('employment',factorProps,dat)
+employment$byAttainment=FIX(dat%>%group_by(deaf,attainCum)%>%do(x=factorProps('employment',.)))
 
-        raceEth=ifelse(hisp>1,"Hispanic",
-                ifelse(rac1p==2,"African American",
-                ifelse(rac1p==6| rac1p==7,"Asian/PacIsl",
-                ifelse(rac1p%in%c(3,4,5),'American Indian',
-                ifelse(rac1p==1,"White","Other"))))),
-
-        diss=ifelse(ddrs==1|deye==1|dout==1|dphy==1|(!is.na(dratx)&dratx==1)|drem==1,1,0),
-        blind=ifelse(deye==1,1,0),
-
-        sex=ifelse(sex==1,'Male','Female'))
+inLaborForce <- lapply(employment,
+                       function(x) setNames(data.frame(x[,seq(ncol(x)-7)],
+                                                       100-x[['% Not In Labor Force']],
+                                                       x[['Not In Labor Force.se']],x$n),
+                                            c(rep('',ncol(x)-7),'% In Labor Force','SE','n')))
 
 
+medianEarnings <-
+    standard(~pernp,med,filter(dat,fulltime),
+             byAttainment=FIX(dat%>%filter(fulltime)%>%group_by(deaf,attainCum)%>%do(x=med(~pernp,sdat=.))),
+             overall=FIX(dat%>%group_by(deaf)%>%do(x=med(~pernp,sdat=.))),
+             employed=FIX(dat%>%filter(employment=='Employed')%>%group_by(deaf)%>%do(x=med(~pernp,sdat=.))),
+             fulltime=FIX(dat%>%filter(fulltime)%>%group_by(deaf)%>%do(x=med(~pernp,sdat=.))))
 
+
+medianEarnings <- lapply(medianEarnings,
+                         function(x) {
+                             names(x)[names(x)=='1'] <- 'Med. Earnings'
+                             names(x)[names(x)=='2'] <- 'SE'
+                             names(x)[names(x)=='3'] <- 'n'
+                             x
+                         })
+
+popBreakdown <- list(
+    percentDeaf=factorProps('deaf',dat),
+    byAge=FIX(dat%>%group_by(deaf)%>%do(x=factorProps('Age',.))),
+    bySex=FIX(dat%>%group_by(deaf)%>%do(x=factorProps('sex',.))),
+    byRace=FIX(dat%>%group_by(deaf)%>%do(x=factorProps('raceEth',.))),
+    byDiss=rbind(
+        FIX2(dat%>%group_by(deaf)%>%do(x=factorProps('diss',.))),
+        FIX2(dat%>%group_by(deaf)%>%do(x=factorProps('blind',.))),
+        FIX2(dat%>%group_by(deaf)%>%do(x=factorProps('selfCare',.))),
+        FIX2(dat%>%group_by(deaf)%>%do(x=factorProps('indLiv',.))),
+        FIX2(dat%>%group_by(deaf)%>%do(x=factorProps('amb',.))),
+        FIX2(dat%>%group_by(deaf)%>%do(x=factorProps('servDis',.))),
+        FIX2(dat%>%group_by(deaf)%>%do(x=factorProps('cogDif',.)))))
+
+
+fodD <- factorProps('degree',filter(dat,deaf=='deaf',attain>='Bachelors degree'))
+fodH <- factorProps('degree',filter(dat,deaf=='hearing',attain>='Bachelors degree'))
+seCol <- grep('.se',names(fodD),fixed=TRUE)
+
+fodD <- cbind(`Deaf %`=c(n=fodD['n'],fodD[-c(seCol,length(fodD))]),
+              `Deaf SE`=c(NA,fodD[seCol]))
+fodH <- cbind(`Hearing %`=c(n=fodH['n'],fodH[-c(seCol,length(fodH))]),
+              `Hearing SE`=c(NA,fodH[seCol]))
+
+popBreakdown$`Field of Degree`=cbind(fodD,fodH)
+
+
+occcodeD <- factorProps('industrycode',filter(dat,deaf=='deaf',fulltime))
+occcodeH <- factorProps('industrycode',filter(dat,deaf=='hearing',fulltime))
+seColOcc <- grep('.se',names(occcodeD),fixed=TRUE)
+
+occcodeD <- cbind(`Deaf %`=c(n=occcodeD['n'],occcodeD[-c(seColOcc,length(occcodeD))]),
+              `Deaf SE`=c(NA,occcodeD[seColOcc]))
+occcodeH <- cbind(`Hearing %`=c(n=occcodeH['n'],occcodeH[-c(seColOcc,length(occcodeH))]),
+              `Hearing SE`=c(NA,occcodeH[seColOcc]))
+
+popBreakdown$`Occupation Category`=cbind(occcodeD,occcodeH)
+
+info <- data.frame(c('Dataset',
+                     'Years',
+                     'Ages',
+                     'Excludes',
+                     'Earnings and Occupational Category',
+                     'Field of Degree'),
+                   c('ACS','2017','25-64','Institutionalized People',
+                     'For full-time employed people only',
+                     'For people with Bachelors degrees or higher'))
+names(info) <- c('','')
+
+attainment$info <- info
+employment$info <- info
+medianEarnings$info <- info
+popBreakdown$info <- info
+
+
+openxlsx::write.xlsx(attainment,'EducatonalAttainment2017.xlsx',colWidths='auto')
+openxlsx::write.xlsx(employment,'employment2017.xlsx',colWidths='auto')
+openxlsx::write.xlsx(medianEarnings,'medianEarnings2017.xlsx',colWidths='auto')
+openxlsx::write.xlsx(popBreakdown,'populationBreakdown2017.xlsx',,colWidths='auto')
