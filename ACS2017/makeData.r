7ed75db9cdc8e5187e912d96eb0449ff3cad4f82
adamSales <asales@utexas.edu>
2019-01-28 18:33:59 -0600

    industry code stuff

--- ACS2017/makeData.r
+++ ACS2017/makeData.r
@@ -8,12 +8,12 @@ states <- read.csv('../../../data/states.csv')
 
 
 
-varNames <- c('ST','AGEP','DDRS','DEAR','DEYE','DOUT','DPHY','DRATX','DREM','FDEARP','ESR','SCHL','RAC1P','HISP','SEX','PERNP','PINCP','SSIP','WKHP','WKW','ADJINC','PWGTP','RELP','FOD1P','NAICSP','OCCP','INDP','COW',paste0('PWGTP',1:80))
+varNames <- c('SERIALNO','ST','AGEP','DDRS','DEAR','DEYE','DOUT','DPHY','DRATX','DREM','FDEARP','ESR','SCHL','RAC1P','HISP','SEX','PERNP','PINCP','SSIP','WKHP','WKW','ADJINC','PWGTP','RELP','FOD1P','NAICSP','OCCP','INDP','COW',paste0('PWGTP',1:80))
 
 
 ctypes <- rep('i',length(varNames))
 names(ctypes) <- varNames
-ctypes[c('NAICSP','FOD1P','OCCP','INDP')] <- 'c'
+ctypes[c('SERIALNO','NAICSP','FOD1P','OCCP','INDP')] <- 'c'
 ctypes$.default <- '_'
 colTypes <- do.call('cols',as.list(ctypes))
 
@@ -25,6 +25,11 @@ dat <- rbind(datA[,varNames],datB[,varNames])
 
 rm(datA,datB); gc()
 
+datHHa <- read_csv('../../../data/byYear/ss17husa.csv',col_types=cols(SERIALNO='c',FPARC='i',.default='_'))
+datHHb <- read_csv('../../../data/byYear/ss17husb.csv',col_types=cols(SERIALNO='c',FPARC='i',.default='_'))
+datHH <- rbind(datHHa,datHHb)
+
+dat <- merge(dat,datHH,by='SERIALNO',all.x=TRUE,all.y=FALSE)
 
 names(dat) <- tolower(names(dat))
 
@@ -65,35 +70,79 @@ dat$fodBig <- fodCat$big[match(dat$fod1p,fodCat$num)]
 
 
 ### Taken from factor labels.R (mark bond):
-dat$industry <- NA
-dat$x <- substring(dat$naicsp, first = 1, last=2)
-dat$industry <- ifelse(dat$x == "11", "Agriculture", dat$industry)
-dat$industry <- ifelse(dat$x == "21", "Extraction", dat$industry)
-dat$industry <- ifelse(dat$x == "22", "Utilities", dat$industry)
-dat$industry <- ifelse(dat$x == "23", "Construction", dat$industry)
-dat$industry <- ifelse(dat$x == "31", "Manufacturing", dat$industry)
-dat$industry <- ifelse(dat$x == "32", "Manufacturing", dat$industry)
-dat$industry <- ifelse(dat$x == "33", "Manufacturing", dat$industry)
-dat$industry <- ifelse(dat$x == "3M", "Manufacturing", dat$industry)
-dat$industry <- ifelse(dat$x == "42", "Wholesale", dat$industry)
-dat$industry <- ifelse(dat$x == "44", "Retail", dat$industry)
-dat$industry <- ifelse(dat$x == "45", "Retail", dat$industry)
-dat$industry <- ifelse(dat$x == "4M", "Retail", dat$industry)
-dat$industry <- ifelse(dat$x == "48", "Transportation", dat$industry)
-dat$industry <- ifelse(dat$x == "49", "Transportation", dat$industry)
-dat$industry <- ifelse(dat$x == "51", "Information services", dat$industry)
-dat$industry <- ifelse(dat$x == "52", "Finance", dat$industry)
-dat$industry <- ifelse(dat$x == "53", "Finance", dat$industry)
-dat$industry <- ifelse(dat$x == "54", "Professional services", dat$industry)
-dat$industry <- ifelse(dat$x == "55", "Professional services", dat$industry)
-dat$industry <- ifelse(dat$x == "56", "Professional services", dat$industry)
-dat$industry <- ifelse(dat$x == "61", "Education", dat$industry)
-dat$industry <- ifelse(dat$x == "62", "Medical", dat$industry)
-dat$industry <- ifelse(dat$x == "71", "Entertainment", dat$industry)
-dat$industry <- ifelse(dat$x == "72", "Entertainment", dat$industry)
-dat$industry <- ifelse(dat$x == "81", "Service", dat$industry)
-dat$industry <- ifelse(dat$x == "92", "GOV/MIL/ADM", dat$industry)
-dat$industry <- ifelse(dat$x == "99", "Unemployed", dat$industry)
+indCode <- read.csv('../naicsCodes.csv') ## copied from data dictionary
+### get three-letter descriptions
+indCode$ind2 <- substr(indCode$ind,1,3)
+
+dat$industry <- indCode$ind2[match(dat$naicsp,indCode$code)]
+
+dat$industry <- plyr::revalue(dat$industry, c(
+AGR ="Agriculture, Forestry, Fishing and Hunting",
+EXT ="Mining, Quarrying, and Oil and Gas Extraction",
+UTL ="Utilities",
+CON ="Construction",
+MFG ="Manufacturing",
+WHL ="Wholesale Trade",
+RET ="Retail Trade",
+TRN ="Transportation and Warehousing",
+INF ="Information",
+FIN ="Finance and Insurance; Real Estate and Rental and Leasing",
+PRF ="Professional Services",
+EDU ="Educational Services",
+MED ="Health Care",
+SCA ="Social Assistance",
+ENT ="Arts, Entertainment, and Recreation; Accommodation and Food Services",
+SRV ="Other Services (except Public Administration)",
+ADM ="Public Administration",
+MIL ="Military"))
+
+### aside: print out 3 most common industries in each classification
+indCode$ind <- as.character(indCode$ind)
+indCode$ind3 <- tolower(substr(indCode$ind, 5, nchar(indCode$ind)))
+
+sink('common industries by category.txt')
+for(ind in unique(na.omit(dat$industry))){
+  cat(ind,'\t')
+  ttt <- table(dat$naicsp[dat$industry==ind])
+  for(i in 1:min(length(ttt),3)) cat(indCode$ind3[indCode$code==names(ttt)[i]],' / ')#%in%names(table(dat$naicsp[dat$industry==ind])[1:3])],collapse='; '),'\n')
+  cat('\n')
+}
+sink()
+
+
+
+
+
+
+## dat$industry <- NA
+## dat$x <- substring(dat$naicsp, first = 1, last=2)
+## dat$industry <- ifelse(dat$x =="11","Agriculture", dat$industry)
+## dat$industry <- ifelse(dat$x =="21","Extraction", dat$industry)
+## dat$industry <- ifelse(dat$x =="22","Utilities", dat$industry)
+## dat$industry <- ifelse(dat$x =="23","Construction", dat$industry)
+## dat$industry <- ifelse(dat$x =="31","Manufacturing", dat$industry)
+## dat$industry <- ifelse(dat$x =="32","Manufacturing", dat$industry)
+## dat$industry <- ifelse(dat$x =="33","Manufacturing", dat$industry)
+## dat$industry <- ifelse(dat$x =="3M","Manufacturing", dat$industry)
+## dat$industry <- ifelse(dat$x =="42","Wholesale", dat$industry)
+## dat$industry <- ifelse(dat$x =="44","Retail", dat$industry)
+## dat$industry <- ifelse(dat$x =="45","Retail", dat$industry)
+## dat$industry <- ifelse(dat$x =="4M","Retail", dat$industry)
+## dat$industry <- ifelse(dat$x =="48","Transportation", dat$industry)
+## dat$industry <- ifelse(dat$x =="49","Transportation", dat$industry)
+## dat$industry <- ifelse(dat$x =="51","Information services", dat$industry)
+## dat$industry <- ifelse(dat$x =="52","Finance", dat$industry)
+## dat$industry <- ifelse(dat$x =="53","Finance", dat$industry)
+## dat$industry <- ifelse(dat$x =="54","Professional services", dat$industry)
+## dat$industry <- ifelse(dat$x =="55","Professional services", dat$industry)
+## dat$industry <- ifelse(dat$x =="56","Professional services", dat$industry)
+## dat$industry <- ifelse(dat$x =="61","Education", dat$industry)
+## dat$industry <- ifelse(dat$x =="62","Medical", dat$industry)
+## dat$industry <- ifelse(dat$x =="71","Entertainment", dat$industry)
+## dat$industry <- ifelse(dat$x =="72","Entertainment", dat$industry)
+## dat$industry <- ifelse(dat$x =="81","Service", dat$industry)
+## dat$industry <- ifelse(dat$x =="92","GOV/MIL/ADM", dat$industry)
+## dat$industry <- ifelse(dat$x =="99","Unemployed", dat$industry)
 
 
 dat$state <- states$abb[match(dat$st,states$x)]
@@ -136,8 +185,9 @@ dat <- dat%>%filter(agep>24,agep<65,relp!=16)%>% ## relp==16 for institutionaliz
             ifelse(attain<'Some college, but less than 1 year','HS Diploma',
             ifelse(attain<'Associates degree','Some College',
             ifelse(attain<'Bachelors degree','Associates',
-            ifelse(attain<'Masters degree','Bachelors','Post-Graduate'))))),
-           levels=c('No HS','HS Diploma','Some College','Associates','Bachelors','Post-Graduate')),
+              ifelse(attain<'Masters degree','Bachelors',
+                ifelse(attain<'Doctorate','Masters/Professional','PhD')))))),
+           levels=c('No HS','HS Diploma','Some College','Associates','Bachelors','Masters/Professional','PhD')),
         employment=factor(ifelse(esr%in%c(1,2,4,5),'Employed',
                    ifelse(esr==3,'Unemployed','Not In Labor Force'))),
 
@@ -145,7 +195,9 @@ dat <- dat%>%filter(agep>24,agep<65,relp!=16)%>% ## relp==16 for institutionaliz
 
         raceEth=ifelse(hisp>1,"Hispanic",
                 ifelse(rac1p==2,"African American",
-                ifelse(rac1p==6| rac1p==7,"Asian/PacIsl",
+                  ifelse(rac1p==6,"Asian",
+                    ifelse(rac1p==7,"PacIsl",
+                      ifelse(rac1p==9,"Multiracial",
                 ifelse(rac1p%in%c(3,4,5),'American Indian',
                 ifelse(rac1p==1,"White","Other"))))),
 
@@ -155,7 +207,9 @@ dat <- dat%>%filter(agep>24,agep<65,relp!=16)%>% ## relp==16 for institutionaliz
         sex=ifelse(sex==1,'Male','Female'),
 
         selfEmp=cow%in%(6:7),
-        bizOwner=cow==7
+        bizOwner=cow==7,
+
+        liveWkids=!is.na(fparc)&(fparc!=4)
 
       )
 
