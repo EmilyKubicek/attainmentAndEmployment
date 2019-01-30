085bb30c03d02fc3104ce8897eb0130a0c50e331
adamSales <asales@utexas.edu>
2019-01-30 10:26:26 -0600

    reorganize, add comments

--- ACS2017/attainment.r
+++ ACS2017/attainment.r
@@ -7,11 +7,20 @@ if(!exists("dat")){
         load('attainmentEmploymentDataACS17.RData')
     } else source('makeData.r')
 }
-
+print(xtabs(~Sex,data=dat))
 
 source('../generalCode/estimationFunctions.r')
 source('../generalCode/median.r')
 
+#################################################################################################
+#### useful general functions
+#################################################################################################
+
+###############
+### translate from dplyr:do to matrix
+###############
+
+#### with significance tests
 FIXsig1 <- function(tib1,med=FALSE){
   stopifnot(nrow(tib1)==2)
   #stopifnot(ncol(tib1)==1)
@@ -50,28 +59,6 @@ FIXsig <- function(tib,med=FALSE){
   out
 }
 
-
-
-FIX <- function(tib){
-    lst <- sapply(tib,is.list)
-
-    out <- NULL
-    for(nn in names(tib)[lst]){
-        out <- round(cbind(out,
-                     do.call('rbind',tib[[nn]])),1)
-    }
-    out <- cbind(tib[,!lst],out)
-    names(out)[1:sum(!lst)] <- ''
-    out
-}
-
-FIX2 <- function(tib){
-    tib <- FIX(tib)
-    out <- t(tib[,-c(1,ncol(tib))])
-    colnames(out) <- tib[,1]
-    out
-}
-
 ttest <- function(m1,m2,s1,s2){
   T <- abs(m1-m2)/sqrt(s1^2+s2^2)
   2*pnorm(-T)
@@ -105,6 +92,33 @@ tests <- function(mat1,mat2){
 }
 
 
+#### without significance tests
+
+FIX <- function(tib){
+    lst <- sapply(tib,is.list)
+
+    out <- NULL
+    for(nn in names(tib)[lst]){
+        out <- round(cbind(out,
+                     do.call('rbind',tib[[nn]])),1)
+    }
+    out <- cbind(tib[,!lst],out)
+    names(out)[1:sum(!lst)] <- ''
+    out
+}
+
+FIX2 <- function(tib){
+    tib <- FIX(tib)
+    out <- t(tib[,-c(1,ncol(tib))])
+    colnames(out) <- tib[,1]
+    out
+}
+
+
+###############
+### standard calculations+translation thereof
+###############
+
 
 stand1 <- function(x,FUN,dat,...)
   list(
@@ -136,31 +150,37 @@ stand2 <- function(s1,med=FALSE){
   out
 }
 
+### general info to append to spreadsheets
+info <- data.frame(c('Dataset: ACS',
+                     'Year: 2017',
+                     'Ages: 25-64',
+                     'Excludes Institutionalized People',
+                     'Occupational Category for full-time employed people only',
+                     'Field of Degree for people with Bachelors degrees or higher'),
+                   stringsAsFactors=FALSE)
+
+names(info) <- c('')
 
 
-## standard <- function(x,FUN,dat,...)
-##   list(
-##     overall=FIXsig(dat%>%group_by(deaf)%>%do(x=FUN(x,.)))#,
-##     byAge=FIXsig(dat%>%group_by(deaf,Age)%>%do(x=FUN(x,.)))#,
-##     bySex=FIXsig(dat%>%group_by(deaf,Sex)%>%do(x=FUN(x,.)))#,
-##     byRace=FIXsig(dat%>%group_by(deaf,raceEth)%>%do(x=FUN(x,.)))#,
-##     byRaceGender=FIXsig(dat%>%group_by(deaf,raceEth,Sex)%>%do(x=FUN(x,.)))#,
-##     byParenthood=FIXsig(dat%>%group_by(deaf,liveWkids)%>%do(x=FUN(x,.)))#,
-##     byDiss=rbind(
-##       FIXsig(dat%>%group_by(deaf,diss)%>%do(x=FUN(x,.)))#,
-##       FIXsig(dat%>%group_by(deaf,blind)%>%do(x=FUN(x,.)))#,
-##       FIXsig(dat%>%group_by(deaf,selfCare)%>%do(x=FUN(x,.)))#,
-##       FIXsig(dat%>%group_by(deaf,indLiv)%>%do(x=FUN(x,.)))#,
-##       FIXsig(dat%>%group_by(deaf,amb)%>%do(x=FUN(x,.)))#,
-##       FIXsig(dat%>%group_by(deaf,servDis)%>%do(x=FUN(x,.)))#,
-##       FIXsig(dat%>%group_by(deaf,cogDif)%>%do(x=FUN(x,.))))#,
-##     ...
-##   )
 
-print(xtabs(~Sex,data=dat))
+#################################################################################################
+#### Educational attainment
+#################################################################################################
 
 attainment1 <- stand1('attainCum',factorProps,dat)
 attainment <- stand2(attainment1)
+
+## write results to spreadsheet
+attainment$info <- info
+openxlsx::write.xlsx(attainment,'EducatonalAttainment2017.xlsx',colWidths='auto')
+
+#################################################################################################
+#### Employment, LFP
+#################################################################################################
+
+###############
+### employment
+###############
 
 empFun <- function(x,.data){
     emp <- factorProps('employment',.data)
@@ -185,6 +205,35 @@ employment1 <- stand1('employment',empFun,dat,
 
 employment <- stand2(employment1)
 
+## write results to spreadsheet
+employment$info <- rbind(info,'Full/Part-time expressed as percentage of employed people')
+openxlsx::write.xlsx(employment,'employment2017.xlsx',colWidths='auto')
+
+###############
+### by industry category
+###############
+employmentByIndustry <- FIX(dat%>%filter(fulltime)%>%group_by(deaf)%>%do(x=factorProps('industry',.)))
+## write results to spreadsheet
+employmentByIndustry <- info <- rbind(info,'Full-time employed people only')
+openxlsx::write.xlsx(list(employmentByIndustry=employmentByIndustry,
+  info=rbind(info,'Full-time employed people only')),
+  'industryPercentagesFT2017.xlsx', rowNames=TRUE,colWidths='auto')
+
+###############
+### employment by age (plot)
+###############
+empByAge <- FIX(dat%>%group_by(deaf,agep)%>%do(x=estExpr(employment=="Employed",sdat=.)))
+names(empByAge)[1:2] <- c('deaf','Age')
+openxlsx::write.xlsx(list(empByAge=empByAge), info=info),'EmploymentByAge2017.xlsx')
+
+ggplot(empByAge,aes(Age,est,color=deaf,group=deaf))+geom_smooth()+labs(color=NULL,y='% Employed')
+ggsave('employmentByAge.jpg')
+
+###############
+## LFP
+###############
+
+### basically just extract info from 'employment'
 inLaborForce <-
   lapply(employment,
     function(x)
@@ -194,6 +243,14 @@ inLaborForce <-
             function(i) ifelse(i%%3==0,x[['% Not In Labor Force']][i],100-x[['% Not In Labor Force']][i]),1.2),
           x[['Not In Labor Force SE']],x$n),
         c(rep('',ncol(x)-11),'% In Labor Force','SE','n')))
+
+## write results to spreadsheet
+inLaborForce$info <- rbind(info,'Calculated from employment numbers')
+openxlsx::write.xlsx(inLaborForce,'LaborForceParticipation2017.xlsx',colWidths='auto')
+
+#################################################################################################
+#### Earnings
+#################################################################################################
 
 
 medianEarnings1 <-
@@ -220,18 +277,32 @@ medianEarnings <- stand2(medianEarnings1,med=TRUE)
 names(medianEarnings)[!names(medianEarnings)%in%c('everyone','employed')] <-
   paste(names(medianEarnings)[!names(medianEarnings)%in%c('everyone','employed')],'(Full-time)')
 
-## medianEarnings <- lapply(medianEarnings,
-##                          function(x) {
-##                              names(x)[names(x)=='1'] <- 'Med. Earnings'
-##                              names(x)[names(x)=='2'] <- 'SE'
-##                              names(x)[names(x)=='3'] <- 'n'
-##                              x
-##                          })
+medianEarnings$info <- rbind(info,c('Earnings are for full-time employed people, except in "everyone" and "employed" tabs'))
+openxlsx::write.xlsx(medianEarnings,'medianEarnings2017.xlsx',colWidths='auto')
 
-employmentByIndustry <- FIX(dat%>%filter(fulltime)%>%group_by(deaf)%>%do(x=factorProps('industry',.)))
 
-############# self employed & business owners
-## percent own small biz
+
+## earnings by age
+ernByAge <- FIX(dat%>%filter(fulltime)%>%group_by(deaf,agep)%>%do(x=med(~pernp,sdat=.)))
+names(ernByAge) <- c('deaf','Age','median_earnings','se','n')
+openxlsx::write.xlsx(ernByAge,'medianEarningsByAgeFullTime2017.xlsx')
+
+ggplot(ernByAge,aes(Age,median_earnings,color=deaf,group=deaf))+geom_smooth()+labs(color=NULL,y='Median Earnings (Full-Time Employed)')
+ggsave('earningsByAge.jpg')
+
+## mean earnings by age
+ernByAgeMean <- FIX(dat%>%filter(fulltime)%>%group_by(deaf,agep)%>%do(x=estSEstr('pernp',sdat=.)))
+names(ernByAgeMean) <- c('deaf','Age','mean_earnings','se','n')
+openxlsx::write.xlsx(ernByAgeMean,'meanEarningsByAgeFullTime2017.xlsx')
+
+ggplot(ernByAgeMean,aes(Age,mean_earnings,color=deaf,group=deaf))+geom_smooth()+geom_point()+labs(color=NULL,y='Mean Earnings (Full-Time Employed)')
+ggsave('earningsByAgeMean.jpg')
+
+
+#################################################################################################
+#### Self employment and business ownership
+#################################################################################################
+
 bizOwner1 <- list(
   overall= dat%>%group_by(deaf)%>%do(x=estExpr(bizOwner,sdat=.)),
   employed=dat%>%filter(employment=='Employed')%>%group_by(deaf)%>%do(x=estExpr(bizOwner,sdat=.)),
@@ -250,8 +321,6 @@ selfEmp1 <- list(
 selfEmp <- do.call('rbind',lapply(selfEmp1,FIXsig,med=TRUE))
 names(selfEmp)[2] <- '% Self Employed'
 
-
-
 medEarnBizOwner1 <- list(
   bizOwner=dat%>%filter(fulltime,bizOwner)%>%group_by(deaf)%>%do(x=med(~pernp,sdat=.)),
   bizOwnerUnder40=dat%>%filter(fulltime,bizOwner,agep<40)%>%group_by(deaf)%>%do(x=med(~pernp,sdat=.)),
@@ -260,7 +329,18 @@ medEarnBizOwner1 <- list(
 medEarnBizOwner <- do.call('rbind',lapply(medEarnBizOwner1,FIXsig,med=TRUE))
 names(medEarnBizOwner) <- c('','Median Earnings','SE','n')
 
-selfEmployment=list(`Owns Business`=bizOwner,`Self Employed`=selfEmp,`Earnings by Self Employment`=medEarnBizOwner)
+selfEmployment=list(`Owns Business`=bizOwner,
+  `Self Employed`=selfEmp,
+  `Earnings by Self Employment`=medEarnBizOwner,
+  info=info)
+
+### write to spreadsheet
+openxlsx::write.xlsx(selfEmployment,'selfEmployment2017.xlsx',rowNames=TRUE,colWidths='auto')
+
+
+#################################################################################################
+#### Population breakdown (i.e. how many ppl in each category)
+#################################################################################################
 
 
 popBreakdown <- list(
@@ -278,6 +358,7 @@ popBreakdown <- list(
         FIX2(dat%>%group_by(deaf)%>%do(x=factorProps('cogDif',.)))))
 
 
+##### field of degree
 fod1D <- factorProps('fodSmall',filter(dat,deaf=='deaf',attain>='Bachelors degree'))
 fod1H <- factorProps('fodSmall',filter(dat,deaf=='hearing',attain>='Bachelors degree'))
 seCol1 <- grep(' SE',names(fod1D),fixed=TRUE)
@@ -300,7 +381,7 @@ fod2H <- cbind(`Hearing %`=c(n=fod2H['n'],fod2H[-c(seCol2,length(fod2H))]),
 
 popBreakdown$`Field of Degree (big)`=round(cbind(fod2D,fod2H),1)
 
-
+##### industry codes
 occcodeD <- factorProps('industry',filter(dat,deaf=='deaf',fulltime))
 occcodeH <- factorProps('industry',filter(dat,deaf=='hearing',fulltime))
 seColOcc <- grep(' SE',names(occcodeD),fixed=TRUE)
@@ -312,59 +393,16 @@ occcodeH <- cbind(`Hearing %`=c(n=occcodeH['n'],occcodeH[-c(seColOcc,length(occc
 
 popBreakdown$`Industry`=round(cbind(occcodeD,occcodeH),1)
 
-info <- data.frame(c('Dataset: ACS',
-                     'Year: 2017',
-                     'Ages: 25-64',
-                     'Excludes Institutionalized People',
-                     'Occupational Category for full-time employed people only',
-                     'Field of Degree for people with Bachelors degrees or higher'),
-                   stringsAsFactors=FALSE)
-
-names(info) <- c('')
-
-attainment$info <- info
-employment$info <- rbind(info,'Full/Part-time expressed as percentage of employed people')
-medianEarnings$info <- rbind(info,c('Earnings are for full-time employed people, except in "everyone" and "employed" tabs'))
-popBreakdown$info <- info
-
 popBreakdown[1:4] <- lapply(popBreakdown[1:4],t)
-inLaborForce$info <- rbind(info,'Calculated from employment numbers')
 
-openxlsx::write.xlsx(inLaborForce,'LaborForceParticipation2017.xlsx',colWidths='auto')
-openxlsx::write.xlsx(attainment,'EducatonalAttainment2017.xlsx',colWidths='auto')
-openxlsx::write.xlsx(employment,'employment2017.xlsx',colWidths='auto')
-openxlsx::write.xlsx(medianEarnings,'medianEarnings2017.xlsx',colWidths='auto')
+### write to spreadsheet
+popBreakdown$info <- info
 openxlsx::write.xlsx(popBreakdown,'populationBreakdown2017.xlsx',rowNames=TRUE,colWidths='auto')
-openxlsx::write.xlsx(employmentByIndustry,'industryPercentagesFT2017.xlsx', rowNames=TRUE,colWidths='auto')
-openxlsx::write.xlsx(selfEmployment,'selfEmployment2017.xlsx',rowNames=TRUE,colWidths='auto')
-
-library(ggplot2)
-
-## employment by age
-empByAge <- FIX(dat%>%group_by(deaf,agep)%>%do(x=estExpr(employment=="Employed",sdat=.)))
-names(empByAge)[1:2] <- c('deaf','Age')
-openxlsx::write.xlsx(empByAge,'EmploymentByAge2017.xlsx')
-
-ggplot(empByAge,aes(Age,est,color=deaf,group=deaf))+geom_smooth()+labs(color=NULL,y='% Employed')
-ggsave('employmentByAge.jpg')
-
-## earnings by age
-ernByAge <- FIX(dat%>%filter(fulltime)%>%group_by(deaf,agep)%>%do(x=med(~pernp,sdat=.)))
-names(ernByAge) <- c('deaf','Age','median_earnings','se','n')
-openxlsx::write.xlsx(ernByAge,'medianEarningsByAgeFullTime2017.xlsx')
-
-ggplot(ernByAge,aes(Age,median_earnings,color=deaf,group=deaf))+geom_smooth()+labs(color=NULL,y='Median Earnings (Full-Time Employed)')
-ggsave('earningsByAge.jpg')
-
-## mean earnings by age
-ernByAgeMean <- FIX(dat%>%filter(fulltime)%>%group_by(deaf,agep)%>%do(x=estSEstr('pernp',sdat=.)))
-names(ernByAgeMean) <- c('deaf','Age','mean_earnings','se','n')
-openxlsx::write.xlsx(ernByAgeMean,'meanEarningsByAgeFullTime2017.xlsx')
-
-ggplot(ernByAgeMean,aes(Age,mean_earnings,color=deaf,group=deaf))+geom_smooth()+geom_point()+labs(color=NULL,y='Mean Earnings (Full-Time Employed)')
-ggsave('earningsByAgeMean.jpg')
 
 
+
+
+### this is mostly messing around
 ### test (log) earnings disparities
 
 mod0 <- lm(log(pernp)~deaf+as.factor(agep)+fodBig,data=dat,weights=pwgtp,subset=pernp>0&attain>="Bachelors degree")
