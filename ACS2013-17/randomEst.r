4fff834c82281112de6d29a92aa04261394527ec
adamSales <asales@utexas.edu>
2019-06-14 10:41:32 -0500

    overall industry percentage

--- ACS2013-17/randomEst.r
+++ ACS2013-17/randomEst.r
@@ -41,14 +41,46 @@ sink()
 
 
 ### by industry category
-employmentByIndustry <- dat25%>%filter(fulltime,blackORwhite=='Black')%>%
+employmentByIndustry <- dat25%>%
+  filter(fulltime)%>%
+  bind_rows(mutate(.,blackORwhite='Overall'))%>%
+  filter(blackORwhite!='Other')%>%
+  group_by(blackORwhite,deaf)%>%
+  group_map(~as.data.frame(rbind(factorProps('industry',.))))
+
+raceTab <- xtabs(~raceEth+blackORwhite,data=dat25)
+blackWith <- raceTab%>%as_tibble%>%
+  filter(blackORwhite=='Black',raceEth!='African American',n>0)%>%
+  pull(raceEth)
+whiteWith <- raceTab%>%as_tibble%>%
+  filter(blackORwhite=='White',raceEth!='White',n>0)%>%
+  pull(raceEth)
+
+ebiInfo <- data.frame(
+  info=
+    c('Years','Ages','Race/Ethnicity Definition:','Black','White','Industry Code Definition','total n'),
+  what=
+    c('2013-2017',
+      paste(min(dat25$agep),'-',max(dat25$agep)),
+      '',
+      paste('Includes Black/African American alone',
+        if(length(blackWith)) paste('Black',blackWith,collapse=', ') else NULL,collapse=','),
+       paste('Includes White alone',
+         if(length(whiteWith)) paste('White',whiteWith,collapse=', ') else NULL,collapse=','),
+      'Industry codes represent a compromise between the NAICS sectors and the abbreviations in the ACS codebook. In practice, they are the NAICS sectors, with "Finance  and  Insurance" and "Real  Estate  and  Rental  and  Leasing" combined, and "Professional, Scientific, and Technical Services", "Management of Companies and Enterprises", and "Administrative  and  Support  and  Waste  Management  and  Remediation  Services" combined into one category'))
+
+
+
+
+
+employmentByIndustryOverall <- dat25%>%filter(fulltime)%>%
   group_by(deaf)%>%do(x=factorProps('industry',.))
 ## write results to spreadsheet
 
-employmentByIndustry <- cbind(employmentByIndustry[,-which(names(employmentByIndustry)=='x')],
-  do.call('rbind',employmentByIndustry$x))
+employmentByIndustry <- setNames(do.call('data.frame',employmentByIndustry$x),employmentByIndustry$deaf)
+employmentByIndustryOverall <- setNames(do.call('data.frame',employmentByIndustryOverall$x),employmentByIndustryOverall$deaf)
 
-ebi <- t(employmentByIndustry[,-grep(' SE',names(employmentByIndustry),fixed=TRUE)])
+
 
 openxlsx::write.xlsx(ebi,
   'output/BlackIndustryPercentagesFT2012-17.xlsx', rowNames=TRUE,colWidths='auto')
